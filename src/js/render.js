ddbad3d779a1de9bb15a9716080fe4bd8906dd4d
ArtemVakulenko <80762228+ArtemVakulenko@users.noreply.github.com>
2021-04-10 23:42:45 +0300

    changed no questions image

--- src/js/render.js
+++ src/js/render.js
@@ -101,7 +101,7 @@ function createAndFillQuestionItem($node, serverData, i) {
           <p><b>Answer:</b> </p><p class="questions__resultInfo">${serverData[i].answer}</p>
         </div>
         <div class="questions__info">
-          <p><b>Date:</b> </p><p class="questions__resultInfo">${serverData[i].stringDate}</p>
+          <p><b>Date: </b> </p><p class="questions__resultInfo">${serverData[i].stringDate}</p>
       </div>
     </div>
   </div>`;
@@ -157,5 +157,4 @@ export function rerenderElement(event, obj) {
   $inputExp.placeholder = obj.exp;
   $hobbie.textContent = obj.hobbie;
   $inputHobbie.placeholder = obj.hobbie;
- 
 }
